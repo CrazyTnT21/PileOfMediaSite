d2628f6525df640e8f1bf57ec136528fd2216623
CrazyTnT <crazytnt21@gmail.com>
2023-12-30 15:56:10 +0100

    Added nginx.conf, async to script tags & improved titles

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,3 +2,9 @@ import {loadColorScheme} from "../classes/color-scheme.js";
 
 //TODO: Prevent flash of unstyled content
 loadColorScheme();
+
+const title = document.querySelector("title");
+if (!title.innerText.includes("MyCollection"))
+{
+  console.warn(`The website title is missing for site '${window.location}'`);
+}
