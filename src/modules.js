c3e8f96c46948ad55ef73af358695738113f623b
CrazyTnT <CrazyTnT21@gmail.com>
2024-03-31 16:17:04 +0200

    Configure webpack to export es6 bundles

---
+++ src/modules.js
@@ -0,0 +1,3 @@
+//This file acts as a re-exporter for node modules
+
+// export * as d3 from "d3";
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,55 @@
-const path = require("path");
-const fs = require("fs");
+import path from "path";
+import fs from "fs";
+import {fileURLToPath} from "url";
 
-module.exports = {
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "src"),
-            staticOptions: {
-                extensions: ["html"],
-            },
-        },
-        historyApiFallback: {
-            rewrites: [
-                {
-                    from: /\//,
-                    to: context => customDirectoryIndex(context)
-                },
-            ],
-        },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-        compress: true,
-        port: 80,
+export default {
+  entry: path.join(__dirname, "src", "modules.js"),
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+      staticOptions: {
+        extensions: ["html"],
+      },
     },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /\//,
+          to: context => customDirectoryIndex(context),
+        },
+      ],
     },
-    mode: "development",
-    target: "web",
+
+    compress: true,
+    port: 80,
+  },
+  output: {
+    filename: "modules.js",
+    path: path.resolve(__dirname, "dist"),
+    library: {
+      type: "module",
+    },
+  },
+
+  experiments: {
+    outputModule: true,
+  },
+
+  mode: "development",
+  target: "web",
 };
 
 function customDirectoryIndex(context)
 {
-    const parts = context.parsedUrl.path.split("/");
-    const filePath = context.parsedUrl.path + parts[parts.length - 2] + ".html";
+  const parts = context.parsedUrl.path.split("/");
+  const filePath = context.parsedUrl.path + parts[parts.length - 2] + ".html";
 
-    if (fs.existsSync(path.join(__dirname, "src", filePath)))
-    {
-        return filePath;
-    }
-    return "404.html";
+  if (fs.existsSync(path.join(__dirname, "src", filePath)))
+  {
+    return filePath;
+  }
+  return "404.html";
 }
