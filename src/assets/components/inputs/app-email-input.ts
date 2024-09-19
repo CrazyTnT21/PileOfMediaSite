52d618f85afefb1ca1d203626c3989917038c313
Daniel Rosenfeld <crazytnt21@gmail.com>
2024-09-19 13:59:53 +0200

    email input

---
+++ src/assets/components/inputs/app-email-input.ts
@@ -0,0 +1,12 @@
+import {AppInput} from "./app-input.js";
+
+export class AppEmailInput extends AppInput
+{
+  constructor()
+  {
+    super();
+    this.shadowRoot!.querySelector("input")!.type = "email";
+  }
+}
+
+customElements.define("app-email-input", AppEmailInput);
